7b08c64681f390e63bb4b5991d4a202666fd0253
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-23 15:21:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,13 +13,13 @@ end
 def get_japanese_emoticon(filepath, emoticon)
   emoticons = load_library(filepath)
   result = emoticons.find{|key , value| value[:english] == emoticon}
-  #binding.pry
+  binding.pry
   return result.nil? ?  "Sorry, that emoticon was not found" : result[1][:japanese]
 end
 
 def get_english_meaning(filepath, emoticon)
   emoticons = load_library(filepath)
   result = emoticons.find{|key , value| value[:japanese] == emoticon}
-  binding.pry
+  #binding.pry
   return result.nil? ?  "Sorry, that emoticon was not found" : result[0]
 end
