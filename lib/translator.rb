3fea3a3548b13c80dc767c174acc16cad0df85a6
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-23 15:13:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,6 @@ end
 def get_english_meaning(filepath, emoticon)
   emoticons = load_library(filepath)
   solution = emoticons.find{|key , value| value[:japanese] == emoticon}
-  solution[1]
+  return solution[0]
   #binding.pry
 end
