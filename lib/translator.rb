e6f232eb863a13db3655c940af844019a42388a6
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-23 15:02:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(filepath)
   emoticons = YAML.load_file(filepath)
   solution = emoticons.each_with_object({}) do |(key , value) , hash|
     hash["#{key}"] = {english: value[0], japanese: value[1]}
-    binding.pry
+    #binding.pry
   end
 end
 
